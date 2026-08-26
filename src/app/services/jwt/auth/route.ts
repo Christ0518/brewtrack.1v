9fe1d837429511681a127e0e4c7f19f78ffe272f
ilanbutasngskyflakes <elilucenio@gmail.com>
2026-08-26 23:10:45 +0800

    Fix production login session

--- src/app/services/jwt/auth/route.ts
+++ src/app/services/jwt/auth/route.ts
@@ -23,7 +23,7 @@ export async function POST(req: NextRequest) {
         name: "token",
         value: token,
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
+        secure: req.nextUrl.protocol === "https:",
         sameSite: "strict",
         path: "/",
         maxAge: 86400,
